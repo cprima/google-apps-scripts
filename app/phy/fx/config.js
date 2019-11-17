757809022b1ac0360d70ac01787ff426c1fb15cc
Christian Prior-Mamulyan <cprior@gmail.com>
2019-11-17 20:16:43 +0100

    Squashed commit of the following:
    commit 6c56c602fecfd6b57faf2bab9ae3e80f491defca
    Author: Christian Prior-Mamulyan <cprior@gmail.com>
    Date:   Sun Nov 17 20:14:08 2019 +0100
    
        Added rudimentary initialization code: If configured sheets don't exist, then create them and fill the header row.
    
    commit 62b7e04c15497705f644f728e375cd9bf5a8e8c7
    Author: Christian Prior-Mamulyan <cprior@gmail.com>
    Date:   Sun Nov 17 19:17:56 2019 +0100
    
        Refactored doCbaam() to use new XMLService calls, as doEcb() does. Added experimental email report.
    
    commit f398ea20460583dc7a493b2285d0adb4354864b1
    Author: Christian Prior-Mamulyan <cprior@gmail.com>
    Date:   Sun Nov 17 11:01:24 2019 +0100
    
        Added code to fetch EUR/USD rate from ECB, implementing new XMLService (which needs to replace doCbaam()'s deprecated XML)

--- app/phy/fx/config.js
+++ app/phy/fx/config.js
@@ -1,14 +1,23 @@
 //general
-//var thisScriptsSheetId = "1phpfJD2aPkxjL5KUg_OMSdJ3q80UtW5YuA34v3mFVjw"
 var output_report_mailaddresses = ["cprior@gmail.com", "arminemamulyan1@gmail.com"];
+var datatemplate = {
+    guid: '',
+    title: '',
+    date: new Date(),
+    currency: '',
+    pair: '',
+    rate: new Number(),
+    multiplier: new Number(),
+    scriptId: '',
+    created_at: new Date()
+};
 
-//cba.am AMD/EUR|USD configuration
+//cba
 var input_cbaam_feedurl = "https://www.cba.am/_layouts/rssreader.aspx?rss=280F57B8-763C-4EE4-90E0-8136C13E47DA";
-//var output_cbaam_spreadsheetId = "1xr01yt47ghoqAjJ6FPMYQ6EIdi8lYM0AXtFBSC8uJQE"
-var output_cbaam_spreadsheetId = "1phpfJD2aPkxjL5KUg_OMSdJ3q80UtW5YuA34v3mFVjw"
+var output_spreadsheetId = "1phpfJD2aPkxjL5KUg_OMSdJ3q80UtW5YuA34v3mFVjw"
 var output_cbaam_sheetname = 'cba.am';
 var process_cbaam_currencies = ["EUR", "USD"];
-var output_cbaam_sheet = SpreadsheetApp;
-var output_cbaam_existingGuids = [];
 
-
+//ecb
+var input_ecbusd_feedurl = "https://www.ecb.europa.eu/rss/fxref-usd.html";
+var output_ecbusd_sheetname = 'ecb';
--- app/phy/fx/main.js
+++ app/phy/fx/main.js
@@ -1,5 +1,5 @@
 /**
- * Automated handling of forex exhange rate processing.
+ * Automated handling of forex exchange rate processing.
  *
  * @author: Christian Prior-Mamulyan <cprior@gmail.com>
  * @license: MIT
@@ -7,13 +7,11 @@
  */
 
 
-//unused
-function init() { return; }
-
 //to be called from triggers
 function main() {
     init();
     doCbaam();
+    doEcb();
 }
 
 //reading RSS feed and persiting to Google Sheet
@@ -26,64 +24,100 @@ function main() {
 </item>
 */
 function doCbaam() {
-    //where to write to
-    output_cbaam_sheet = SpreadsheetApp.openById(output_cbaam_spreadsheetId).getSheetByName(output_cbaam_sheetname);
-    //if entry does not exist already
-    output_cbaam_existingGuids = getExistingCbaamGuids(output_cbaam_sheet);
 
     //read RSS
-    var items = getFeedContentParsed(input_cbaam_feedurl)
-    for (var i in items) {
-        guid = myGetElementText(items[i], "guid") //need this first to check for duplicates
-        currency = myGetNthElementText(items[i], "title", " - ", 0); //need this to process only configured currencies
+    root = getFeedDocument(input_cbaam_feedurl);
+    //get the items of interest
+    var items = root.getChild('channel').getChildren('item');
 
-        //insertIfNotExists
-        if (process_cbaam_currencies.indexOf(currency) >= 0 && existingGuids.indexOf(guid) == -1) {
-            output_cbaam_sheet.appendRow([guid,
-                myGetElementText(items[i], "title"),
-                new Date(myGetElementText(items[i], "pubDate")), //date
-                currency,
-                parseFloat(myGetNthElementText(items[i], "title", " - ", 2)), //rate
-                myGetNthElementText(items[i], "title", " - ", 1), //multiplier
-                ScriptApp.getScriptId(),
-                new Date()]);
-            //reportIf
-            for (var j in output_report_mailaddresses) {
-                try {
-                    MailApp.sendEmail(
-                        output_report_mailaddresses[j], //to
-                        currency + ": " + myGetNthElementText(items[i], "title", " - ", 2), //subject
-                        "" //body
-                    );
-                } catch (e) {
-                    Logger.log(e)
-                }
-            }
+    for (var i in items) {
+
+        data = datatemplate;
+
+        data.id = items[i].getChild('guid').getValue();
+        data.title = items[i].getChild('title').getText();
+        data.date = new Date(items[i].getChild('pubDate').getText());
+        data.rate = parseFloat(myGetNthElementFromSplitText(items[i], "title", " - ", 2));
+        data.multiplier = myGetNthElementFromSplitText(items[i], "title", " - ", 1);
+        data.currency = myGetNthElementFromSplitText(items[i], "title", " - ", 0);
+
+        //attempt to insert only selected currencies
+        if (process_cbaam_currencies.indexOf(data.currency) >= 0) {
+            appendRowIfNotExists(output_spreadsheetId, output_cbaam_sheetname, data, 'AMD');
         }
     }
 }
 
-function myGetNthElementText(item, elementname, splitstring, position) {
-    return item.getElement(elementname).getText().split(splitstring)[position];
+//reading RSS feed and persiting to Google Sheet
+/* sample item
+<item rdf:about="http://www.ecb.europa.eu/stats/exchange/eurofxref/html/eurofxref-graph-usd.en.html?date=2019-11-15&amp;rate=1.1034">
+<title xml:lang="en">1.1034 USD = 1 EUR 2019-11-15 ECB Reference rate</title>
+<link>http://www.ecb.europa.eu/stats/exchange/eurofxref/html/eurofxref-graph-usd.en.html?date=2019-11-15&amp;rate=1.1034</link>
+<description xml:lang="en">1 EUR buys 1.1034 US dollar (USD) - The reference exchange rates are published both by electronic market information providers and on the ECB's website shortly after the concertation procedure has been completed. Reference rates are published according to the same  calendar as the TARGET system.</description>
+<dc:date>2019-11-15T14:15:00+01:00</dc:date>
+<dc:language>en</dc:language>
+<cb:statistics>
+<cb:country>U2</cb:country>
+<cb:institutionAbbrev>ECB</cb:institutionAbbrev>
+<cb:exchangeRate>
+<cb:value frequency="daily" decimals="4">1.1034</cb:value>
+<cb:baseCurrency unit_mult="0">EUR</cb:baseCurrency>
+<cb:targetCurrency>USD</cb:targetCurrency>
+<cb:rateType>Reference rate</cb:rateType>
+</cb:exchangeRate>
+</cb:statistics>
+</item>
+*/
+//@see: https://www.ecb.europa.eu/rss/fxref-usd.html
+function doEcb() {
+
+    var ns = XmlService.getNamespace('http://purl.org/rss/1.0/');
+    var dc = XmlService.getNamespace('http://purl.org/dc/elements/1.1/');
+    var cb = XmlService.getNamespace('http://www.cbwiki.net/wiki/index.php/Specification_1.1');
+    var rdf = XmlService.getNamespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+
+    //read RSS
+    root = getFeedDocument(input_ecbusd_feedurl);
+
+    //get the items of interest
+    var items = root.getChildren('item', ns);
+
+    for (var i = 0; i < items.length; i++) {
+        data = datatemplate;
+
+        data.id = items[i].getAttribute('about', rdf).getValue();
+        data.title = items[i].getChild('title', ns).getText();
+        data.date = new Date(items[i].getChild('date', dc).getText());
+        data.rate = parseFloat(items[i].getChild('statistics', cb)
+            .getChild('exchangeRate', cb)
+            .getChild('value', cb).getValue());
+        data.multiplier = Math.pow(10, parseInt(items[i]
+            .getChild('statistics', cb)
+            .getChild('exchangeRate', cb)
+            .getChild('baseCurrency', cb)
+            .getAttribute('unit_mult')
+            .getValue()));
+        data.currency = items[i].getChild('statistics', cb).getChild('exchangeRate', cb).getChild('targetCurrency', cb).getValue();
+
+        //attempt to insert
+        appendRowIfNotExists(output_spreadsheetId, output_ecbusd_sheetname, data, 'EUR');
+
+        //Logger.log('%s (%s) from %s with %s for %s times %s', data.title, data.id, data.date, data.rate, data.currency, data.multiplier);
+    }
+
 }
 
-function myGetElementText(item, elementname) {
-    return item.getElement(elementname).getText();
+function myGetNthElementFromSplitText(item, elementname, splitstring, position) {
+    return item.getChild(elementname).getText().split(splitstring)[position];
 }
 
-
-
-//
-function getFeedContentParsed(url) {
-    var feed = Xml.parse(UrlFetchApp.fetch(url).getContentText(), false);
-    return feed.getElement().getElement("channel").getElements("item");
-}
-
-//
-function getExistingCbaamGuids(sheet) {
+//return all values in a single column as a list
+//used as poor mans UNIQUE to check which data already exists
+function getColumnValues(sheet, colNum) {
+    if (colNum == undefined) { colNum = 1 }
     //read all existing guid values to prevent duplicates later
     //getRange(row, column, numRows, numColumns)
-    var temp = sheet.getRange(1, 1, sheet.getLastRow(), 1).getValues();
+    var temp = sheet.getRange(1, colNum, sheet.getLastRow(), 1).getValues();
     existingGuids = [];
     for (var i in temp) {
         if (i >= 1) {//dont need the header row
@@ -93,9 +127,79 @@ function getExistingCbaamGuids(sheet) {
     return existingGuids
 }
 
-//
-function test() {
-    Logger.log(sheetId)
-    Logger.log("test")
+//returns the parsed XML from a RSS feed location
+function getFeedDocument(url) {
+    return XmlService.parse(UrlFetchApp.fetch(url).getContentText()).getRootElement();
 }
 
+function appendRowIfNotExists(spreadsheetId, sheetName, data, baseCurrency) {
+
+    //where to write to
+    sheet = SpreadsheetApp
+        .openById(spreadsheetId)
+        .getSheetByName(sheetName);
+
+    //get all existing entries
+    existingIdentifiers = getColumnValues(sheet);
+
+    if (existingIdentifiers.indexOf(data.id) == -1) {
+        sheet.appendRow([data.id,
+        data.title,
+        data.date,
+        data.currency,
+        baseCurrency + '/' + data.currency,
+        data.rate,
+        data.multiplier,
+        ScriptApp.getScriptId(),
+        new Date()]);
+    }
+}
+
+//https://stackoverflow.com/a/22200230/9576512
+function emailReport() {
+    var charts = SpreadsheetApp.openById(output_spreadsheetId).getSheetByName('vizAMD-USD-EUR').getCharts();
+    Logger.log(charts);
+    if (charts.length > 0) {
+
+        var chartBlobs = new Array(charts.length);
+        var emailBody = "Charts<br>";
+        var emailImages = {};
+        for (var i = 0; i < charts.length; i++) {
+            chartBlobs[i] = charts[i].getAs("image/png").setName("chartBlob" + i);
+            emailBody = emailBody + "<img src='cid:chart" + i + "'><br>";
+            emailImages["chart" + i] = chartBlobs[i];
+        }
+
+        MailApp.sendEmail({
+            to: 'cprior@gmail.com',
+            subject: 'chart',
+            htmlBody: emailBody,
+            inlineImages: emailImages
+        });
+
+
+    }
+}
+
+
+function init() {
+
+    tocheck = [output_cbaam_sheetname, output_ecbusd_sheetname]
+
+    for (var i = 0; i < tocheck.length; i++) {
+
+        if (!SpreadsheetApp.openById(output_spreadsheetId).getSheetByName(tocheck[i])) {
+
+            SpreadsheetApp.openById(output_spreadsheetId).insertSheet(tocheck[i], 0);
+
+            j = 1;
+            for (var column in datatemplate) {
+                SpreadsheetApp.openById(output_spreadsheetId).getSheetByName(tocheck[i]).getRange(1, j).setValue(column);
+                j++;
+            }
+
+        }
+
+    }
+
+}
